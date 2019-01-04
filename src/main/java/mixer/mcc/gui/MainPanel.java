0f3bd6c5b221aca999a59fda7a22101f4643ce57
d33pfr13d <naplike@gmail.com>
2019-01-04 21:34:40 +0100

    vlc connector prototype

--- src/main/java/mixer/mcc/gui/MainPanel.java
+++ src/main/java/mixer/mcc/gui/MainPanel.java
@@ -9,38 +9,64 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import mixer.mcc.services.twitter.TwitterBot;
+import mixer.mcc.services.vlc.VlcConnector;
 import net.miginfocom.swing.MigLayout;
 
 public class MainPanel extends JPanel {
 	
 	//TODO Das kommt alles noch in ne controller klasse
 	private TwitterBot twitterBot = new TwitterBot();
+	
+	private VlcConnector vlc = new VlcConnector();
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6829793510139914776L;
-	
+
 	public MainPanel() {
 		super(new MigLayout());
 		
 		final JTextArea jtLive = new JTextArea("Ich streame JETZT live auf Mixer. Schaut doch mal rein: http://mixer.com/d33pfr13d");
-		JButton jbLive = new JButton("LIVE GEHEN");
+		JButton jbLive = new JButton("(L)IVE GEHEN");
 		jbLive.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String liveTxt = jtLive.getText();
 				
+				//TODO usually that would be a command being executed...
 				twitterBot.tweet(liveTxt);
 			}
 		});
 		
 		jbLive.setMnemonic(KeyEvent.VK_L);
 		
+		//TODO Wahrscheinlich würde man das konfiguerbar haben wollen, wobei man
+		//überhaupt alle actions konfigurierbar haben will und nicht feste buttons!!!
+		// -> generisches "Play Video Command" das man dann per properties x-mal spawnen kann
+		// Man braucht auch keinen settings dialog, ne props file finde ich viel komfortabler ^^
+		final JTextArea jtVideo = new JTextArea("D:\\data\\Programme\\Telegram\\media\\on-a-boat-clipped.mp4");
+		JButton jbBoat = new JButton("ON A (B)OAT");
+		jbBoat.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				//TODO usually that would be a command being executed...
+				vlc.playVideo(jtVideo.getText());
+			}
+		});
+		jbBoat.setMnemonic(KeyEvent.VK_B);
+		
 		
 		add(jtLive);
-		add(jbLive);
+		add(jbLive,"wrap");
+		
+		add(jtVideo);
+		add(jbBoat);
+		
+		
 		
 		
 	}
---
+++ src/main/java/mixer/mcc/services/vlc/VlcConnector.java
@@ -0,0 +1,44 @@
+package mixer.mcc.services.vlc;
+
+import java.io.IOException;
+
+import jonas.tools.execution.CommandLineUtils;
+
+/**
+ * Connects the mcc to your local vlc player.
+ * 
+ * The vision is to have playback control and everyhting via a cli api, not sure
+ * if vlc offers all that, we might have to switch to mplayer which I have used
+ * that way in the past.
+ * 
+ * For starters we just start videos and make vlc close after, so we can add a
+ * capture input to slobs that shows the active vlc window ;-)
+ * 
+ * 
+ * @author d33pfr13d
+ *
+ */
+public class VlcConnector {
+	
+	/**
+	 * TODO Das soll aus ner properties Datei kommen, wir brauchen dafür ein Konfigurations-Modul.
+	 * 
+	 * TODO FIXEM KOMMT NICHT MIT SPACES KLAR -> MÜSSTE RUNTIME.EXEC in JUT mit array aufrufen!!!
+	 * -> workaorund mit mklink /J VLC <ACTUAL-PATH> nen kurzen pfad erstellen in cmd!!!
+	 */
+	private static String VLC_PATH = "D:\\Programme\\VLC\\";
+	
+	private CommandLineUtils cli = new CommandLineUtils();
+	
+	public void playVideo(String videoPath) {
+		try {
+			cli.executeCommand("cmd /k "+ VLC_PATH+"vlc.exe --play-and-exit \""+videoPath+"\"");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	
+
+}
